3ebef4b37b464244a45063d43ce5818b9204fbfb
Lemonsoft-creator <lemonsoft1990@gmail.com>
2025-09-12 09:54:32 +0200

    Delete components/ui/pages.tsx

--- components/ui/pages.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import { cn } from '../utils';
-export function Badge({ className, ...props }: React.HTMLAttributes<HTMLSpanElement>) {
-  return <span className={cn('inline-flex items-center px-2 py-0.5 rounded-full text-xs font-medium bg-slate-100 text-slate-700', className)} {...props} />;
-}
